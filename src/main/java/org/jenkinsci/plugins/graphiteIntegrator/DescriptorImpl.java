50adf96d14f2dd9c1fa44b66a7fad0b0532bb2d6
Michael Dumont <Michael.Dumont@blackboard.com>
2014-02-19 17:47:02 -0500

    jobType is not the same in jenkins; update DescriptorImpl to make available as post-build notifier

--- src/main/java/org/jenkinsci/plugins/graphiteIntegrator/DescriptorImpl.java
+++ src/main/java/org/jenkinsci/plugins/graphiteIntegrator/DescriptorImpl.java
@@ -92,7 +92,7 @@ public final class DescriptorImpl extends BuildStepDescriptor<Publisher> impleme
 	 */
 	@Override
 	public boolean isApplicable(Class<? extends AbstractProject> jobType) {
-		return "hudson.maven.MavenModuleSet".equals(jobType.getName());
+		return true;
 	}
 
 	/*
